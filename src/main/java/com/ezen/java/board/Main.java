7dedaf5c64118dec169084129df8fcf4a5b3fbb2
kimhonghyun <xode132@gmail.com>
2024-08-26 14:57:01 +0900

    기본 명령어 처리

--- src/main/java/com/ezen/java/board/Main.java
+++ src/main/java/com/ezen/java/board/Main.java
@@ -1,9 +1,16 @@
 package com.ezen.java.board;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("== 게시판 작동 ==");
-        System.out.println("게시판을 시작합니다");
-        System.out.println("== 게시판 종료 ==");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("== 자바 텍스트 게시판 작동 ==");
+        System.out.print("명령) ");
+        String cmd = sc.nextLine();
+
+        System.out.println("입력받은 명령어 : " + cmd);
+        System.out.println("== 자바 텍스트 게시판 종료 ==");
     }
 }
