ae5d584827fa2863310f98cae361a7ed40beb097
kimhonghyun <xode132@gmail.com>
2024-08-26 19:42:55 +0900

    lastArticle의 필요성을 제거

--- src/main/java/com/ezen/java/board/Main.java
+++ src/main/java/com/ezen/java/board/Main.java
@@ -15,7 +15,6 @@ public class Main {
     }
     public static void main(String[] args) {
         int lastArticleId = 0;
-        Article lastArticle = null;
         List<Article> articles = new ArrayList<>();
 
         makeTestData(articles);
@@ -43,25 +42,24 @@ public class Main {
                 int id = ++lastArticleId;
 
                 Article article = new Article(id, subject, content); //게시물 객체 생성
-                lastArticle = article;
 
                 articles.add(article);
                 System.out.printf("%d번 게시물이 등록되었습니다.\n", article.id);
             }else if(cmd.equals("/usr/article/detail")) {
 
-                Article article = lastArticle;
-
-                if (article == null) {
+                if (articles.isEmpty()) {
                     System.out.println("게시물이 존재하지 않습니다.");
                     continue;
                 }
+
+                Article article = articles.get(articles.size() - 1);
 
                 System.out.println("==게시물 상세보기 ==");
                 System.out.printf("번호 : %d\n", article.id);
                 System.out.printf("제목 : %s\n", article.subject);
                 System.out.printf("내용 : %s\n", article.content);
             }else if(cmd.equals("/usr/article/list")){
-                if(articles.size() == 0) {
+                if(articles.isEmpty()) {
                     System.out.println("게시물이 존재하지 않습니다.");
                     continue;
                 }
