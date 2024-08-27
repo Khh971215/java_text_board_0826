56c32b2262653575e55cb44de6f3c021149ad2f5
kimhonghyun <xode132@gmail.com>
2024-08-27 15:55:00 +0900

    Map을 이용하여 파라미터 분석하여 데이터 저장

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,12 +1,13 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         // 문제점 : 현재 우리 파싱로직은 유연하지 못한다.
         // 왜? 파라미터가 추가되면 추가 된 내용에 대해서 추가 작업을 해줘야 하기 때문에..
-        String queryString = "hit=73&id=3&memberId=13&boardId=2";
+        String queryString = "id=3&memberId=13&boardId=2&hit=73&writerName=신짱구";
+
+        Map<String, String> params = new LinkedHashMap<>();
+        // LinkedHashMap : 순서를 보장
 
         // 압축해제 시작
         String[] queryStrignBits = queryString.split("&");
@@ -15,22 +16,30 @@ public class Main {
             System.out.println(bit);
         }
         */
-        List<String> paramNames = new ArrayList<>();
-        List<Integer> paramValues = new ArrayList<>();
+
 
         for(String bit : queryStrignBits) {
             String[] bitBits = bit.split("=");
-            String paramName = bitBits[0];
-            String paramValue = bitBits[1];
+            params.put(bitBits[0], bitBits[1]);
 
-            paramNames.add(paramName);
-            paramValues.add(Integer.parseInt(paramValue));
+
 
             // System.out.printf("%s : %s\n", paramName, paramValue);
         }
         // 압축해제 끝
-        System.out.println(paramNames);
-        int boardIdIndex = paramNames.indexOf("boardId");
-        System.out.println("boardId : " + paramValues.get(boardIdIndex));
+        System.out.println("== 원하는 것을 하나하나 가져와서 사용 ==");
+        System.out.printf("id : %d\n", Integer.parseInt(params.get("id")));
+        System.out.printf("memberId : %d\n", Integer.parseInt(params.get("memberId")) );
+        System.out.printf("boardId : %d\n", Integer.parseInt(params.get("boardId")));
+        System.out.printf("hit : %d\n", Integer.parseInt(params.get("hit")));
+        System.out.printf("writerName : %s\n", params.get("writerName"));
+
+
+        System.out.println("== 반복문을 이용한 순회 출력 ==");
+        for(String paramName : params.keySet()) {
+            String paramValue = params.get(paramName);
+
+            System.out.printf("%s : %s\n", paramName, paramValue);
+        }
     }
 }
