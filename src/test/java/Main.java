3f63dd8eab66fe62be04d285aca35f9665c8a1b3
kimhonghyun <xode132@gmail.com>
2024-08-27 16:01:38 +0900

    파싱 로직을 복사 붙여넣기 했을때 불필요한 중복이 발생한다

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -11,12 +11,6 @@ public class Main {
 
         // 압축해제 시작
         String[] queryStrignBits = queryString.split("&");
-        /*
-        for(String bit : queryStrignBits) {
-            System.out.println(bit);
-        }
-        */
-
 
         for(String bit : queryStrignBits) {
             String[] bitBits = bit.split("=");
@@ -27,17 +21,36 @@ public class Main {
             // System.out.printf("%s : %s\n", paramName, paramValue);
         }
         // 압축해제 끝
-        System.out.println("== 원하는 것을 하나하나 가져와서 사용 ==");
-        System.out.printf("id : %d\n", Integer.parseInt(params.get("id")));
-        System.out.printf("memberId : %d\n", Integer.parseInt(params.get("memberId")) );
-        System.out.printf("boardId : %d\n", Integer.parseInt(params.get("boardId")));
-        System.out.printf("hit : %d\n", Integer.parseInt(params.get("hit")));
-        System.out.printf("writerName : %s\n", params.get("writerName"));
+
+        String queryString2 = "id=1&memberId=3&boardId=3&hit=53";
+
+        Map<String, String> params2 = new LinkedHashMap<>();
+        // LinkedHashMap : 순서를 보장
+
+        // 압축해제 시작
+        String[] queryStrignBits2 = queryString2.split("&");
+
+        for(String bit : queryStrignBits2) {
+            String[] bitBits = bit.split("=");
+            params2.put(bitBits[0], bitBits[1]);
+
+
+
+            // System.out.printf("%s : %s\n", paramName, paramValue);
+        }
+        // 압축해제 끝
 
 
         System.out.println("== 반복문을 이용한 순회 출력 ==");
         for(String paramName : params.keySet()) {
             String paramValue = params.get(paramName);
+
+            System.out.printf("%s : %s\n", paramName, paramValue);
+        }
+
+        System.out.println("== 반복문을 이용한 순회 출력2 ==");
+        for(String paramName : params2.keySet()) {
+            String paramValue = params2.get(paramName);
 
             System.out.printf("%s : %s\n", paramName, paramValue);
         }
